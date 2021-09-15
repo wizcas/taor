79c60863c758934a362a6bf4c7cfe5b48457a460
Wizcas Chen <chen@0x1c.dev>
2021-09-16 00:57:40 +0800

    remove logs

--- server/api/wallhaven/search.js
+++ server/api/wallhaven/search.js
@@ -3,8 +3,6 @@ import { wallhavenAPI } from "./const";
 
 export default async function search(req, res) {
     const { query } = req;
-    console.log({ query });
-    console.log({ req });
     const data = await http.get(wallhavenAPI.search, { params: query });
     res.status(200).json(data);
 }
