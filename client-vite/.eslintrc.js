c5842767f66164a7e58d39e05becddae90a0a8b7
wizcas <chen@0x1c.dev>
2021-11-03 21:24:14 +0800

    fixed linting

--- client-vite/.eslintrc.js
+++ client-vite/.eslintrc.js
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -13,5 +19,15 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    'no-shadow': 'off',
+    'no-use-before-define': 'off',
+
+    '@typescript-eslint/no-use-before-define': ['error'],
+
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'import/extensions': [2, { ts: 'never', tsx: 'never' }],
+  },
 };
--- client-vite/src/main.tsx
+++ client-vite/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// eslint-disable-next-line import/no-unresolved
 import 'virtual:windi.css';
 
 ReactDOM.render(
