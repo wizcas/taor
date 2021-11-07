68f2edfc05a347560ecebdc3c0fdcd3c3468c41a
wizcas <chen@0x1c.dev>
2021-11-07 22:48:22 +0800

    page modal style

--- client/src/components/modals/PageModal.tsx
+++ client/src/components/modals/PageModal.tsx
@@ -2,9 +2,9 @@ import Modal from 'react-modal';
 import { observer } from 'mobx-react-lite';
 import { useLocation } from 'react-router-dom';
 import { ReactNode } from 'react';
-import usePageModal from '@/hooks/usePageModal';
 import ModalHeader from './ModalHeader';
 import styles from './PageModal.module.css';
+import usePageModal from '@/hooks/usePageModal';
 
 interface Props {
   title: ReactNode;
@@ -18,7 +18,12 @@ function Component(props: Props) {
   const isOpen = !!location.pathname && location.pathname !== '/';
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={close} className={styles.modal}>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={close}
+      className={styles.modal}
+      overlayClassName={styles.overlay}
+    >
       <ModalHeader title={title} onClose={close} />
       <div className={styles.content}>{children}</div>
     </Modal>
