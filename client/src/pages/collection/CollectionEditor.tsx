a1e953ab4ed8da6ed77e4e83caf1aae4dbfea474
wizcas <chen@0x1c.dev>
2021-11-21 21:57:20 +0800

    rename collection in the editor

--- client/src/pages/collection/CollectionEditor.tsx
+++ client/src/pages/collection/CollectionEditor.tsx
@@ -3,10 +3,12 @@ import { observer } from 'mobx-react-lite';
 import Modal from '@/components/modals/Modal';
 import { CollectionsContext } from '@/providers';
 import { ModalRef } from '@/types';
+import EditableTextBox from '@/components/form/EditableTextBox';
 
 export default observer(function CollectionEditor() {
   const [isOpen, setIsOpen] = useState(false);
   const collections = useContext(CollectionsContext);
+  const collection = collections.editingCollection;
 
   const close = useCallback(() => {
     setIsOpen(false);
@@ -23,8 +25,19 @@ export default observer(function CollectionEditor() {
     return () => collections.disposeEditor(modal);
   }, [collections, close]);
 
+  function onSubmitName(value: string) {
+    if (!collection) return;
+    collections.rename(collection, value);
+  }
+
+  const title = (
+    <div className="flex gap-2 items-center">
+      <EditableTextBox value={collection?.name} onSubmit={onSubmitName} />
+    </div>
+  );
+
   return (
-    <Modal isOpen={isOpen} onRequestClose={close}>
+    <Modal isOpen={isOpen} onRequestClose={close} title={title}>
       Editing {collections.editingCollection?.name}
     </Modal>
   );
